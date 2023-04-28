ed72053eac0dc396c1f59e91822bdc6fa2123ecb
Shivam <201b251@juetguna.in>
2023-04-29 00:02:17 +0530

    I have added Drop Down list for country and year wise analysis

--- app.py
+++ app.py
@@ -2,16 +2,29 @@ import streamlit as st
 import pandas as pd
 import preprocessor,helper
 
-df = preprocessor.preprocess()
+df = pd.read_csv('athlete_events.csv')
+region_df = pd.read_csv('noc_regions.csv')
+
+df = preprocessor.preprocess(df,region_df)
+
+st.sidebar.title("Olympic Analysis")
 
 user_menu = st.sidebar.radio(
     'select an Option',
     ('Medal Tally','Overall Analysis','Country-wise Analysis','Athlete wise Analysis')
 )
  
-st.dataframe(df)
 
-if user_
+
+if user_menu == 'Medal Tally':
+    st.sidebar.header("Medal Tally")
+    years,country = helper.country_year_list(df)
+
+    selected_year = st.sidebar.selectbox("Select Year",years)
+    selected_country = st.sidebar.selectbox("Select Country",country)
+    
+    medal_tally = helper.medal_tally(df)
+    st.dataframe(medal_tally)
 
 
  
